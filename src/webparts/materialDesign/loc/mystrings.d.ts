275045b6a50479cd327bf3daf2355345f04a7556
Kelvin <kelvin.kiprop96@gmail.com>
2019-03-05 13:23:46 +0300

    Initial commit

---
+++ src/webparts/materialDesign/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMaterialDesignWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MaterialDesignWebPartStrings' {
+  const strings: IMaterialDesignWebPartStrings;
+  export = strings;
+}
